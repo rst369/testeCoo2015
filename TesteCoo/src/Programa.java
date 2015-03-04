dbd2f3276f3ba5d050e0a081a52bcdc42a3ec2a3
Torres <rst_369@hotmail.com>
2015-03-04 15:12:55 -0300

    terceiro Commit

--- TesteCoo/src/Programa.java
+++ TesteCoo/src/Programa.java
@@ -1,7 +1,7 @@
 class Programa {
 public static void main(String[] args) {
 Conta umaConta = new Conta();
-umaConta.titular = "Duke";
+umaConta.titular = "Rafael";
 umaConta.saldo = 1000.0;
 System.out.println("Saldo atual: " + umaConta.saldo);
 }
